d520ed7fdc19604af9598dd869b97618b5117f05
jose <jose.s.contacto@gmail.com>
2023-11-18 00:49:02 -0300

    add lead script

---
+++ lead.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Lead: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'jose.s.contacto@gmail.com';
+$asunto = '¡Nuevo lead en OP!';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
